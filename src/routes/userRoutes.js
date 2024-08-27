9308f2e357f2d90061b642762ae2a36defc49360
luis revilla <lrev4766@gmail.com>
2024-08-26 22:10:35 -0500

    Add user routes for user-related actions

---
+++ src/routes/userRoutes.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const userController = require("../controllers/userController");
+const { protect } = require("../middleware/authMiddleware");
+
+const router = express.Router();
+
+router.get("/profile", protect, userController.getProfile);
+router.put("/profile", protect, userController.updateProfile);
+
+module.exports = router;
