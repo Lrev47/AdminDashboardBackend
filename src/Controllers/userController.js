12498182649f9eb9b58d714386e886f776f277a9
luis revilla <lrev4766@gmail.com>
2024-08-26 22:10:35 -0500

    Add UserController to handle CRUD operations for users

---
+++ src/Controllers/userController.js
@@ -0,0 +1,28 @@
+const prisma = require("../services/prismaClient");
+
+const userController = {
+  getProfile: async (req, res, next) => {
+    try {
+      const user = await prisma.user.findUnique({
+        where: { id: req.user.userId },
+      });
+      res.json(user);
+    } catch (error) {
+      next(error);
+    }
+  },
+
+  updateProfile: async (req, res, next) => {
+    try {
+      const updatedUser = await prisma.user.update({
+        where: { id: req.user.userId },
+        data: req.body,
+      });
+      res.json(updatedUser);
+    } catch (error) {
+      next(error);
+    }
+  },
+};
+
+module.exports = userController;
