9e2adf4156f1734a8431047061afc2d16a8bc0fa
luis revilla <lrev4766@gmail.com>
2024-08-27 22:50:31 -0500

    Update authRoutes.js

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const authController = require("../controllers/authController");
+const authController = require("../Controllers/authController.js");
 
 const router = express.Router();
 
