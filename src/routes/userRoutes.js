3fc8948e90c50efbecab2933b1e2406df4b52d31
luis revilla <lrev4766@gmail.com>
2024-08-27 22:50:31 -0500

    Update userRoutes.js

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const userController = require("../controllers/userController");
+const userController = require("../Controllers/userController");
 const { protect } = require("../middleware/authMiddleware");
 
 const router = express.Router();
