9b7aaa26b314bb772fff71d104a333109eb66139
luis revilla <lrev4766@gmail.com>
2024-08-28 22:01:26 -0500

    Add main route index file

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,55 @@
 const express = require("express");
-const authRoutes = require("./authRoutes");
-const userRoutes = require("./userRoutes");
-const adminRoutes = require("./adminRoutes");
+const alertsRoutes = require("./alerts-routes");
+const analyticsRoutes = require("./analytics-routes");
+const authRoutes = require("./auth-routes");
+const mockDataRoutes = require("./mock-data-routes");
+const notificationsRoutes = require("./notifications-routes");
+const permissionsRoutes = require("./permissions-routes");
+const contentRoutes = require("./content-routes");
+const dbQueryRoute = require("./db-queries-routes");
+const environmentRoutes = require("./environment-routes");
+const externalServicesRoutes = require("./external-services-routes");
+const financeRoutes = require("./finance-routes");
+const imageRoutes = require("./images-routes");
+const projectRoutes = require("./project-routes");
+const settingRoutes = require("./settings-routes");
+const testandDebugRoutes = require("./testing-and-debugging-routes");
+const userRoutes = require("./users-routes");
+// const versionControlRoutes = require("./version-control-routes");
 
-const router = express.Router();
+const app = express();
 
-router.use("/auth", authRoutes);
-router.use("/user", userRoutes);
-router.use("/admin", adminRoutes);
+// Middleware to parse JSON bodies
+app.use(express.json());
 
-module.exports = router;
+// Use the routes
+app.use("/alerts", alertsRoutes);
+app.use("/analytics", analyticsRoutes);
+app.use("/auth", authRoutes);
+app.use("/mock-data", mockDataRoutes);
+app.use("/notifications", notificationsRoutes);
+app.use("/permissions", permissionsRoutes);
+app.use("/contnent", contentRoutes);
+app.use("/dbQuery", dbQueryRoute);
+app.use("/environment", environmentRoutes);
+app.use("/externalServices", externalServicesRoutes);
+app.use("/finance", financeRoutes);
+app.use("/image", imageRoutes);
+app.use("/project", projectRoutes);
+app.use("/settings", settingRoutes);
+app.use("/testandDebug", testandDebugRoutes);
+app.use("/user", userRoutes);
+// app.use("/", versionControlRoutes);
+
+// Handle 404 errors
+app.use((req, res, next) => {
+  res.status(404).json({ message: "Route not found" });
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ message: "Internal Server Error" });
+});
+
+module.exports = app;
